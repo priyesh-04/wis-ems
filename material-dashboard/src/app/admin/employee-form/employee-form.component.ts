77b714abaee628d62935beb4e245854e5d030c47
Saumyadeb <SBiswas@reds.com>
2023-07-05 19:27:01 +0530

    fixed some coding issue

--- material-dashboard/src/app/admin/employee-form/employee-form.component.ts
+++ material-dashboard/src/app/admin/employee-form/employee-form.component.ts
@@ -1,11 +1,12 @@
-import { Component, Inject, OnInit, Input } from "@angular/core";
-import { FormGroup, FormBuilder, Validators, FormControl } from "@angular/forms";
+import { Component, Inject, OnInit } from "@angular/core";
+import { FormGroup, FormBuilder, Validators } from "@angular/forms";
+import { MatDialogRef, MAT_DIALOG_DATA } from "@angular/material/dialog";
+
 import {
   validatorIndianMobileNumber,
   validatorEmail,
   validatorTextOnly,
 } from "../../utils/custom-validators";
-import { MatDialogRef, MAT_DIALOG_DATA } from "@angular/material/dialog";
 import { EmployeeListComponent } from "../employee-list/employee-list.component";
 import { DesignationService } from "../../services/designation/designation.service";
 import { EmployeeService } from "../../services/employee/employee.service";
@@ -18,20 +19,18 @@ import { ClientService } from "../../services/client/client.service";
   styleUrls: ["./employee-form.component.css"],
 })
 export class EmployeeFormComponent implements OnInit {
-  employeeForm: FormGroup;
-  employeeData: any;
-  clientList:[];
-  designationList: any;
-  selectedDesignation: any;
-  selectedRole: any;
-  selectedClients : any;
-  selectedHolidays : any;
-  isAdmin :boolean;
-  roleList = [
+  public employeeForm: FormGroup;
+  public clientList = [];
+  public designationList = [];
+  public selectedClients: any;
+  public selectedHolidays: any;
+  public isAdmin: boolean;
+  public roleList = [
     { value: "employee", viewValue: "Employee" },
     { value: "hr", viewValue: "HR" },
   ];
-  holidayList: string[] = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+  public holidayList = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+
   constructor(
     private _employeeService: EmployeeService,
     private _designationService: DesignationService,
@@ -54,33 +53,23 @@ export class EmployeeFormComponent implements OnInit {
       assigned_client: ["", [Validators.required]],
       holidays: ["", [Validators.required]]
     });
+
+    this.getDesignationList();
+    this.getClientList();
+
     if (this.employeeDialogData.role === 'admin') {
       this.isAdmin = true    
     }
     if (this.employeeDialogData.mode === "edit") {
-      this.getDesignationList();
-      this.getClientList();
-      this.selectedDesignation =
-        this.employeeDialogData.employeeData.designation._id;
       if (this.employeeDialogData.employeeData.role === "admin") {
         this.roleList.push({ value: "admin", viewValue: "Admin" });
-      }
-          
-      this.selectedClients = this.employeeDialogData.employeeData.assigned_client.filter(function (el) {
-        console.log(el, 'el');
-        
-        return el._id
-      });
-      // this.selectedClients = this.clientId;
-      this.selectedHolidays = this.employeeDialogData.employeeData.holidays.forEach(el =>{
-        el
-      }); 
-      console.log(this.selectedClients, 'this.selectedClients');
-      console.log(this.selectedHolidays, 'this.selectedHolidays');
-      
+      }      
+      if (this.employeeDialogData.employeeData?.assigned_client) {
+        this.selectedClients = this.employeeDialogData.employeeData.assigned_client.filter(el => {
+          return el._id;
+        });
+      }      
       this.selectedHolidays = this.employeeDialogData.employeeData.holidays;
-      console.log(this.employeeDialogData.employeeData,'main data');      
-      this.selectedRole = this.employeeDialogData.employeeData.role;
       this.employeeForm.patchValue({
         emp_id: this.employeeDialogData.employeeData.emp_id,
         name: this.employeeDialogData.employeeData.name,
@@ -89,23 +78,16 @@ export class EmployeeFormComponent implements OnInit {
         address: this.employeeDialogData.employeeData.address,
         designation: this.employeeDialogData.employeeData.designation._id,
         role: this.employeeDialogData.employeeData.role,
-        assigned_client:[{
+        assigned_client: [{
           _id :"648183c002532e19d3ea921a"
-        }         
-        ],
+        }],
         holidays: [
           [0, 6]
-        ],
-        
+        ],        
       });
-      
-      document.getElementById("password-div").style.display = "none";
-    } else if (this.employeeDialogData.mode === "add") {
-      this.getDesignationList();
-      this.getClientList();
-      document.getElementById("password-div").style.display = "block";
     }
   }
+
   private getClientList() {
     this._clientService.getClientList().subscribe(
       (res) => {
@@ -116,33 +98,33 @@ export class EmployeeFormComponent implements OnInit {
       }
     );
   }
-  closeDialog() {
-    this.dialogRef.close();
-  }
 
-  get myEmployeeForm() {
-    return this.employeeForm.controls;
-  }
-
-  getDesignationList() {
+  private getDesignationList() {
     this._designationService.getDesignationList().subscribe(
       (res) => {
         this.designationList = res.result;
       },
       (err) => {
-        this._mesgageService.showError(err.message);
+        this._mesgageService.showError(err.message || 'Unable to fetch designation list');
       }
     );
   }
 
-  onSubmit(employeeForm: FormGroup) {
-    this.employeeData = employeeForm.value;
-    console.log(this.employeeData);
+  public closeDialog() {
+    this.dialogRef.close();
+  }
+
+  public get myEmployeeForm() {
+    return this.employeeForm.controls;
+  }
+
+  public onSubmit(employeeForm: FormGroup) {
+    const employeeData = employeeForm.value;
     if (this.employeeDialogData.mode === "edit") {
       this._employeeService
         .updateEmployee(
           this.employeeDialogData.employeeData._id,
-          this.employeeData
+          employeeData
         )
         .subscribe(
           (res) => {
@@ -154,7 +136,7 @@ export class EmployeeFormComponent implements OnInit {
           }
         );
     } else if (this.employeeDialogData.mode === "add") {
-      this._employeeService.addNewEmployee(this.employeeData).subscribe(
+      this._employeeService.addNewEmployee(employeeData).subscribe(
         (res) => {
           this.dialogRef.close("success");
           this._mesgageService.showSuccess(res.message);
