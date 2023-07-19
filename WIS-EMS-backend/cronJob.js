57a01a8d14248d55ef34dff346db8bbdcdc02c2f
Mihir0000 <mihirbaidya2000@gmail.com>
2023-07-19 14:05:55 +0530

    remove unused code

--- WIS-EMS-backend/cronJob.js
+++ WIS-EMS-backend/cronJob.js
@@ -47,60 +47,6 @@ const cronFunction = () => {
     true,
     'Asia/Kolkata'
   ).start();
-
-  // job.start();
-  // reset timesheet permission
-  // cron.schedule(
-  //   '0 30 7 * * *',
-  //   () => {
-  //     TimesheetCron.resetTimesheetEditPermission();
-  //     console.log('All Timesheet edit permission Ended.');
-  //   },
-  //   {
-  //     scheduled: true,
-  //     timezone: 'Asia/Kolkata',
-  //   }
-  // );
-  // //once edited -> edited status
-  // // end of the day
-  // cron.schedule(
-  //   '5 30 7 * * *',
-  //   () => {
-  //     TimesheetCron.changeAcceptedPermission();
-  //     console.log('All Accepted Permission goes to Edited');
-  //   },
-  //   {
-  //     scheduled: true,
-  //     timezone: 'Asia/Kolkata',
-  //   }
-  // );
-  // // holiday or not submission created
-  // // run end of the day
-  // cron.schedule(
-  //   '50 59 23 * * *',
-  //   () => {
-  //     TimesheetCron.createHolidayTimesheet();
-  //     console.log('Creating Holiday sheet');
-  //   },
-  //   {
-  //     scheduled: true,
-  //     timezone: 'Asia/Kolkata',
-  //   }
-  // );
-  // // create official holidays
-  // // run start at start of the day
-  // // run at 0 10 0 * * *
-  // cron.schedule(
-  //   '0 10 0 * * *',
-  //   () => {
-  //     TimesheetCron.createOfficalHolidayTimesheet();
-  //     console.log('Creating Official Holiday sheet');
-  //   },
-  //   {
-  //     scheduled: true,
-  //     timezone: 'Asia/Kolkata',
-  //   }
-  // );
 };
 
 module.exports = cronFunction;
