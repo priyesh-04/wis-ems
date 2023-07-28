0dae918058e8a8a1083c3ff0ad6b99a4cc6654fb
Shrabanti Dhara <shrabanti.dhara90@gmail.com>
2023-07-28 17:49:22 +0530

    fixed date,date-time populating issue in edit mode in admin and employee & change date-time format in employee timesheet creation

--- material-dashboard/src/app/utils/custom-validators.ts
+++ material-dashboard/src/app/utils/custom-validators.ts
@@ -29,6 +29,20 @@ export function getFormattedDate(date) {
   // return [year, month, day].join("-");
 }
 
+export function formatDateToDDMMYYYY(dateString) {
+  const dateObj = new Date(dateString);
+
+  const day = dateObj.getUTCDate().toString().padStart(2, '0');
+  const month = (dateObj.getUTCMonth() + 1).toString().padStart(2, '0');
+  const year = dateObj.getUTCFullYear();
+
+  //return `${day}-${month}-${year}`;
+  return `${year}-${month}-${day}`;
+}
+
+
+
+
 export function getFormattedDatetime(dateString) {
   const d = new Date(dateString);
   let month = "" + (d.getMonth() + 1);
